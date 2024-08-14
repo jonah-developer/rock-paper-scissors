7a1e66b9a29a68420d00d72eb984564307838704
Jonah <jonahdevelopers@gmail.com>
2024-08-14 13:18:33 +0100

    Add function to simulate round

--- script.js
+++ script.js
@@ -12,3 +12,36 @@ function getComputerChoice() {
 function getHumanChoice() {
     return prompt("Rock, Paper or Scissors?").toLowerCase();
 }
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === "rock") {
+        if (computerChoice === "paper") {
+            console.log("You lose! Paper beats Rock");
+        } else if (computerChoice === "scissors") {
+            console.log("You won! Rock beats Scissors")
+        } else {
+            console.log("It's a tie!");
+        }
+    } else if (humanChoice === "paper") {
+        if (computerChoice === "scissors") {
+            console.log("You lose! Scissors beats Paper");
+        } else if (computerChoice === "rock") {
+            console.log("You win! Paper beats Rock");
+        } else {
+            console.log("It's a tie!");
+        }
+    } else if (humanChoice === "scissors") {
+        if (computerChoice === "rock") {
+            console.log("You lose! Rock beats Scissors");
+        } else if (computerChoice === "paper") {
+            console.log("You win! Scissors beats Paper");
+        } else {
+            console.log("It's a tie!");
+        }
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
